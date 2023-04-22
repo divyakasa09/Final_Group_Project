9374b47448d6899be1cb4005ce669a4e9b542e8c
Thranathi Reddy Chada <chada.t@northeastern.edu>
2023-04-21 22:49:44 -0400

    Added pharmacy folder

---
+++ HospitalManagementSystem/src/main/java/Pharmacy/Medicine.java
@@ -0,0 +1,79 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pharmacy;
+
+import java.util.Date;
+
+/**
+ *
+ * @author thran
+ */
+public class Medicine {
+    public static int count=0;
+    String serialNumber;
+    String medicineName;
+    Date expiryDate;
+    boolean isAvailable;
+    int availableCount;
+    float price;
+    
+    public Medicine(String medicineName) {
+        this.serialNumber = "M-" + count++;
+        this.medicineName=medicineName;
+       
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getMedicineName() {
+        return medicineName;
+    }
+
+    public void setMedicineName(String medicineName) {
+        this.medicineName = medicineName;
+    }
+
+    public Date getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(Date expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+    public boolean isIsAvailable() {
+        return isAvailable;
+    }
+
+    public void setIsAvailable(boolean isAvailable) {
+        this.isAvailable = isAvailable;
+    }
+
+    public int getAvailableCount() {
+        return availableCount;
+    }
+
+    public void setAvailableCount(int availableCount) {
+        this.availableCount = availableCount;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+    
+    
+    
+    
+}
--- HospitalManagementSystem/src/main/java/Pharmacy/Pharmacy.java
+++ HospitalManagementSystem/src/main/java/Pharmacy/Pharmacy.java
@@ -4,10 +4,47 @@
  */
 package Pharmacy;
 
+import Role.PharmacistRole;
+import UserAccount.UserAccount;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *
  * @author divya
  */
-public class Pharmacy {
+public class Pharmacy extends UserAccount {
+    
+    String pharmacistName;
+    String email;
+    String contact;
+    
+
+    public String getPharmacistName() {
+        return pharmacistName;
+    }
+
+    public void setPharmacistName(String pharmacistName) {
+        this.pharmacistName = pharmacistName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
     
 }
+
+
