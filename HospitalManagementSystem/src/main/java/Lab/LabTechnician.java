6dadd0ecdec2886a7d538f6e062c2e5acfd85c49
Thranathi Reddy Chada <chada.t@northeastern.edu>
2023-04-21 22:47:30 -0400

    added lab folder

---
+++ HospitalManagementSystem/src/main/java/Lab/LabTechnician.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Lab;
+
+import UserAccount.UserAccount;
+
+/**
+ *
+ * @author thran
+ */
+public class LabTechnician  extends UserAccount {
+    String technicianName;
+
+    public String getTechnicianName() {
+        return technicianName;
+    }
+
+    public void setTechnicianName(String technicianName) {
+        this.technicianName = technicianName;
+    }
+   
+    
+}
